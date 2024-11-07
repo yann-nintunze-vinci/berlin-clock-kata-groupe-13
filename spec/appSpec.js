3ec7d7d36976e04969b3048641aa17362767d5e5
Kyle <kyle.cansse@student.vinci.be>
2024-11-07 18:20:32 +0100

    ADD : Doit renvoyer 'OOOO' lorsque la minute est '00'

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,1 +1,13 @@
 import { Main } from '../src/app.js';
+
+describe('Berlin Clock', function() {
+    const main = new Main();
+
+    //ligne minutes simples
+    it("Doit renvoyer 'OOOO' lorsque la minute est '00' ", function() {
+        const result = main.getLumieresMinutesSimples(0);
+
+        expect(result).toBe('OOOO');
+    });
+
+});
--- src/app.js
+++ src/app.js
@@ -1,3 +1,16 @@
+//const date = new Date();
+//const minutes = date.getMinutes();
+
 export class Main {
+    
+
+
+
+
+    getLumieresMinutesSimples(minute) {
+        if(minute === 0) return 'OOOO';
+    }
+
+
     
 }
