4075eaa7009de74130186ace68d3e5d9f7aa6657
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-11-07 19:08:36 +0100

    edit: indentation

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -1,43 +1,42 @@
-import { Main } from '../src/app.js';
+import { Main } from "../src/app.js";
 
-describe('Berlin Clock', function() {
-    const main = new Main();
+describe("Berlin Clock", function () {
+  const main = new Main();
 
-    //ligne minutes simples
-    it("Doit renvoyer 'OOOO' lorsque la minute est '00' ", function() {
-        const result = main.getLumieresMinutesSimples(0);
+  //ligne minutes simples
+  it("Doit renvoyer 'OOOO' lorsque la minute est '00' ", function () {
+    const result = main.getLumieresMinutesSimples(0);
 
-        expect(result).toBe('OOOO');
-    });
+    expect(result).toBe("OOOO");
+  });
 
-    it("Doit renvoyer 'OOOO' lorsque la minute est '10' ", function() {
-        const result = main.getLumieresMinutesSimples(10);
+  it("Doit renvoyer 'OOOO' lorsque la minute est '10' ", function () {
+    const result = main.getLumieresMinutesSimples(10);
 
-        expect(result).toBe('OOOO');
-    });
-    
+    expect(result).toBe("OOOO");
+  });
 
-    it("Doit renvoyer 'JOOO' lorsque la minute est '11' ", function() {
-        const result = main.getLumieresMinutesSimples(11);
+  it("Doit renvoyer 'JOOO' lorsque la minute est '11' ", function () {
+    const result = main.getLumieresMinutesSimples(11);
 
-        expect(result).toBe('JOOO');
-    });
+    expect(result).toBe("JOOO");
+  });
 
-    it("Doit renvoyer 'JJOO' lorsque la minute est '22' ", function() {
-        const result = main.getLumieresMinutesSimples(22);
+  it("Doit renvoyer 'JJOO' lorsque la minute est '22' ", function () {
+    const result = main.getLumieresMinutesSimples(22);
 
-        expect(result).toBe('JJOO');
-    });
-    it("Doit renvoyer 'JJJO' lorsque la minute est '33' ", function() {
-        const result = main.getLumieresMinutesSimples(33);
+    expect(result).toBe("JJOO");
+  });
 
-        expect(result).toBe('JJJO');
-    });
-    it("Doit renvoyer 'JJJJ' lorsque la minute est '44' ", function() {
-        const result = main.getLumieresMinutesSimples(44);
+  it("Doit renvoyer 'JJJO' lorsque la minute est '33' ", function () {
+    const result = main.getLumieresMinutesSimples(33);
 
-        expect(result).toBe('JJJJ');
-    });
-   
+    expect(result).toBe("JJJO");
+  });
 
+  it("Doit renvoyer 'JJJJ' lorsque la minute est '44' ", function () {
+    const result = main.getLumieresMinutesSimples(44);
+
+    expect(result).toBe("JJJJ");
+  });
 });
--- src/app.js
+++ src/app.js
@@ -2,39 +2,31 @@
 //const minutes = date.getMinutes();
 
 export class Main {
-    
+  getLumieresMinutesSimples(minute) {
+    if (this.estMultipleDe5(minute)) return "OOOO";
+    if (this.leResteVaut4(minute)) return "JJJJ";
+    if (this.leResteVaut3(minute)) return "JJJO";
+    if (this.leResteVaut2(minute)) return "JJOO";
+    if (this.leResteVaut1(minute)) return "JOOO";
+  }
 
+  estMultipleDe5(number) {
+    return number % 5 === 0;
+  }
 
+  leResteVaut1(number) {
+    return number % 5 === 1;
+  }
 
+  leResteVaut2(number) {
+    return number % 5 === 2;
+  }
 
-    getLumieresMinutesSimples(minute) {
-        if(this.estMultipleDe5(minute)) return 'OOOO';
-        if(this.leResteVaut4(minute)) return 'JJJJ';
-        if(this.leResteVaut3(minute)) return 'JJJO';
-        if(this.leResteVaut2(minute)) return 'JJOO';
-        if(this.leResteVaut1(minute)) return 'JOOO';
-    }
+  leResteVaut3(number) {
+    return number % 5 === 3;
+  }
 
-    estMultipleDe5(number) {
-        return number % 5 === 0;
-    }
-
-    leResteVaut1(number) {
-        return number % 5 === 1;
-    }
-    leResteVaut2(number) {
-        return number % 5 === 2;
-    }
-    leResteVaut3(number) {
-        return number % 5 === 3;
-    }
-    leResteVaut4(number) {
-        return number % 5 === 4;
-    }
-
-
-
-
-
-    
+  leResteVaut4(number) {
+    return number % 5 === 4;
+  }
 }
