e29b7263252ecc4bc241651f6dfaa890d241017b
yann-nintunze-vinci <yann.nintunze@student.vinci.be>
2024-11-12 14:21:58 +0100

    add: affichage de l'horloge

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -57,135 +57,133 @@ describe("Berlin Clock", function () {
 
   it("Doit renvoyer 'JOOOOOOOOOO' lorsque la minute est 5", function () {
     const result = main.getLumieresBlocDe5Minutes(5);
-    
+
     expect(result).toBe("JOOOOOOOOOO");
   });
 
   it("Doit renvoyer 'JOOOOOOOOOO' lorsque la minute est 6", function () {
     const result = main.getLumieresBlocDe5Minutes(6);
-    
+
     expect(result).toBe("JOOOOOOOOOO");
   });
 
   it("Doit renvoyer 'JJRJJRJJRJJ' lorsque la minute est 55", function () {
     const result = main.getLumieresBlocDe5Minutes(55);
-    
+
     expect(result).toBe("JJRJJRJJRJJ");
   });
 
   it("Doit renvoyer 'JJRJJRJJRJJ' lorsque la minute est 59", function () {
     const result = main.getLumieresBlocDe5Minutes(59);
-    
+
     expect(result).toBe("JJRJJRJJRJJ");
   });
 
   //ligne heures simples
 
-    it("Doit renvoyer 'OOOO' lorsque l'heure est '0' ", function () {
-        const result = main.getLumieresHeuresSimples(0);
-    
-        expect(result).toBe("OOOO");
-    });
+  it("Doit renvoyer 'OOOO' lorsque l'heure est '0' ", function () {
+    const result = main.getLumieresHeuresSimples(0);
 
-    it("Doit renvoyer 'ROOO' lorsque l'heure est '1' ", function () {
-        const result = main.getLumieresHeuresSimples(1);
-    
-        expect(result).toBe("ROOO");
-    });
+    expect(result).toBe("OOOO");
+  });
 
-    it("Doit renvoyer 'RROO' lorsque l'heure est '2' ", function () {
-        const result = main.getLumieresHeuresSimples(2);
-    
-        expect(result).toBe("RROO");
-    });
+  it("Doit renvoyer 'ROOO' lorsque l'heure est '1' ", function () {
+    const result = main.getLumieresHeuresSimples(1);
 
-    it("Doit renvoyer 'RRRO' lorsque l'heure est '3' ", function () {
-        const result = main.getLumieresHeuresSimples(3);
-    
-        expect(result).toBe("RRRO");
-    });
+    expect(result).toBe("ROOO");
+  });
 
-    it("Doit renvoyer 'RRRR' lorsque l'heure est '4' ", function () {
-        const result = main.getLumieresHeuresSimples(4);
-    
-        expect(result).toBe("RRRR");
-    });
+  it("Doit renvoyer 'RROO' lorsque l'heure est '2' ", function () {
+    const result = main.getLumieresHeuresSimples(2);
 
-    it("Doit renvoyer 'OOOO' lorsque l'heure est '5' ", function () {
-        const result = main.getLumieresHeuresSimples(5);
-    
-        expect(result).toBe("OOOO");
-    });
+    expect(result).toBe("RROO");
+  });
 
-    it("Doit renvoyer 'RRRO' lorsque l'heure est '13' ", function () {
-        const result = main.getLumieresHeuresSimples(13);
-    
-        expect(result).toBe("RRRO");
-    });
+  it("Doit renvoyer 'RRRO' lorsque l'heure est '3' ", function () {
+    const result = main.getLumieresHeuresSimples(3);
+
+    expect(result).toBe("RRRO");
+  });
+
+  it("Doit renvoyer 'RRRR' lorsque l'heure est '4' ", function () {
+    const result = main.getLumieresHeuresSimples(4);
+
+    expect(result).toBe("RRRR");
+  });
+
+  it("Doit renvoyer 'OOOO' lorsque l'heure est '5' ", function () {
+    const result = main.getLumieresHeuresSimples(5);
+
+    expect(result).toBe("OOOO");
+  });
+
+  it("Doit renvoyer 'RRRO' lorsque l'heure est '13' ", function () {
+    const result = main.getLumieresHeuresSimples(13);
+
+    expect(result).toBe("RRRO");
+  });
 
   // Lignes de Blocs de 5 heures
 
-    it("Doit renvoyer 'OOOO' lorsque l'heure est '0' ", function () {
-      const result = main.getLumieresBlocDe5Heures(0);
+  it("Doit renvoyer 'OOOO' lorsque l'heure est '0' ", function () {
+    const result = main.getLumieresBlocDe5Heures(0);
 
-      expect(result).toBe("OOOO");
-    });
+    expect(result).toBe("OOOO");
+  });
 
-    it("Doit renvoyer 'ROOO' lorsque l'heure est '5' ", function () {
-      const result = main.getLumieresBlocDe5Heures(5);
-      
-      expect(result).toBe("ROOO");
-    });
+  it("Doit renvoyer 'ROOO' lorsque l'heure est '5' ", function () {
+    const result = main.getLumieresBlocDe5Heures(5);
 
-    it("Doit renvoyer 'RROO' lorsque l'heure est 10", function () {
-      const result = main.getLumieresBlocDe5Heures(10);
+    expect(result).toBe("ROOO");
+  });
 
-      expect(result).toBe("RROO");
-    });
+  it("Doit renvoyer 'RROO' lorsque l'heure est 10", function () {
+    const result = main.getLumieresBlocDe5Heures(10);
 
-    it("Doit renvoyer 'RRRO' lorsque l'heure est 15", function () {
-      const result = main.getLumieresBlocDe5Heures(15);
+    expect(result).toBe("RROO");
+  });
 
-      expect(result).toBe("RRRO");
-    });
+  it("Doit renvoyer 'RRRO' lorsque l'heure est 15", function () {
+    const result = main.getLumieresBlocDe5Heures(15);
 
-    it("Doit renvoyer 'RRRR' lorsque l'heure est 20", function () {
-      const result = main.getLumieresBlocDe5Heures(20);
+    expect(result).toBe("RRRO");
+  });
 
-      expect(result).toBe("RRRR");
-    });
+  it("Doit renvoyer 'RRRR' lorsque l'heure est 20", function () {
+    const result = main.getLumieresBlocDe5Heures(20);
 
-    it("Doit renvoyer 'RRRR' lorsque l'heure est 23 et 'RRRO' pour les heures simples", function () {
-      const result = main.getLumieresBlocDe5Heures(23);
-      const result2 = main.getLumieresHeuresSimples(23);
-      expect(result).toBe("RRRR");
-      expect(result2).toBe("RRRO");
-    });
+    expect(result).toBe("RRRR");
+  });
 
-    // lumière des secondes
-    it("Doit renvoyer 'R' lorsque les secondes sont à 0", function () {
-      const result = main.getLumieresSecondes(0);
-  
-      expect(result).toBe("R");
-    });
+  it("Doit renvoyer 'RRRR' lorsque l'heure est 23 et 'RRRO' pour les heures simples", function () {
+    const result = main.getLumieresBlocDe5Heures(23);
+    const result2 = main.getLumieresHeuresSimples(23);
+    expect(result).toBe("RRRR");
+    expect(result2).toBe("RRRO");
+  });
 
-    it("Doit renvoyer 'O' lorsque les secondes sont à 1", function () {
-      const result = main.getLumieresSecondes(1);
-  
-      expect(result).toBe("O");
-    });
+  // lumière des secondes
+  it("Doit renvoyer 'R' lorsque les secondes sont à 0", function () {
+    const result = main.getLumieresSecondes(0);
 
-    it("Doit renvoyer 'R' lorsque les secondes sont à 2", function () {
-      const result = main.getLumieresSecondes(2);
-  
-      expect(result).toBe("R");
-    });
+    expect(result).toBe("R");
+  });
 
-    it("Doit renvoyer 'O' lorsque les secondes sont à 43", function () {
-      const result = main.getLumieresSecondes(43);
-  
-      expect(result).toBe("O");
-    });
+  it("Doit renvoyer 'O' lorsque les secondes sont à 1", function () {
+    const result = main.getLumieresSecondes(1);
 
-    
+    expect(result).toBe("O");
+  });
+
+  it("Doit renvoyer 'R' lorsque les secondes sont à 2", function () {
+    const result = main.getLumieresSecondes(2);
+
+    expect(result).toBe("R");
+  });
+
+  it("Doit renvoyer 'O' lorsque les secondes sont à 43", function () {
+    const result = main.getLumieresSecondes(43);
+
+    expect(result).toBe("O");
+  });
 });
--- src/app.js
+++ src/app.js
@@ -1,56 +1,70 @@
-
-
 export class Main {
-  
-
   startClock() {
-      setInterval(() => {
-        let date = new Date();
-        let minutes = date.getMinutes();
-        let heures = date.getHours();
-        let secondes = date.getSeconds();
+    setInterval(() => {
+      let date = new Date();
+      let minutes = date.getMinutes();
+      let heures = date.getHours();
+      let secondes = date.getSeconds();
 
-        console.log(date);
-        //console.log(this.getLumieresSecondes(secondes));
-        //console.log(this.getLumieresBlocDe5Heures(heures));
-        //console.log(this.getLumieresHeuresSimples(heures));
-        //console.log(this.getLumieresBlocDe5Minutes(minutes));
-        //console.log(this.getLumieresMinutesSimples(minutes));
-      }, 1000);
-    
+      console.log(date);
+      console.log(this.getLumieresSecondes(secondes));
+      console.log(this.getLumieresBlocDe5Heures(heures));
+      console.log(this.getLumieresHeuresSimples(heures));
+      console.log(this.getLumieresBlocDe5Minutes(minutes));
+      console.log(this.getLumieresMinutesSimples(minutes));
+    }, 1000);
   }
 
-  
   getLumieresMinutesSimples(minute) {
     const blocs = ["OOOO", "JOOO", "JJOO", "JJJO", "JJJJ"];
     return blocs[this.resteDivisionPar5(minute)];
   }
 
   getLumieresBlocDe5Minutes(minute) {
-      const blocs = ["OOOOOOOOOOO", "JOOOOOOOOOO", "JJOOOOOOOOO", "JJROOOOOOOO", "JJRJOOOOOOO", "JJRJJOOOOOO", "JJRJJROOOOO", "JJRJJRJOOOO", "JJRJJRJJOOO", "JJRJJRJJROO", "JJRJJRJJRJO", "JJRJJRJJRJJ"];
-      return blocs[this.quotientDivisionPar5(minute)];
+    const blocs = [
+      "OOOOOOOOOOO",
+      "JOOOOOOOOOO",
+      "JJOOOOOOOOO",
+      "JJROOOOOOOO",
+      "JJRJOOOOOOO",
+      "JJRJJOOOOOO",
+      "JJRJJROOOOO",
+      "JJRJJRJOOOO",
+      "JJRJJRJJOOO",
+      "JJRJJRJJROO",
+      "JJRJJRJJRJO",
+      "JJRJJRJJRJJ",
+    ];
+    return blocs[this.quotientDivisionPar5(minute)];
   }
 
   getLumieresHeuresSimples(heure) {
-      const blocs = ["OOOO","ROOO","RROO", "RRRO", "RRRR"];
-      return blocs[this.resteDivisionPar5(heure)];
+    const blocs = ["OOOO", "ROOO", "RROO", "RRRO", "RRRR"];
+    return blocs[this.resteDivisionPar5(heure)];
   }
 
-  getLumieresBlocDe5Heures(heure){
-    const blocs = ["OOOO","ROOO","RROO","RRRO","RRRR"];
+  getLumieresBlocDe5Heures(heure) {
+    const blocs = ["OOOO", "ROOO", "RROO", "RRRO", "RRRR"];
     return blocs[this.quotientDivisionPar5(heure)];
   }
 
-  getLumieresSecondes(secondes){
+  getLumieresSecondes(secondes) {
     const blocs = ["R", "O"];
     return blocs[secondes % 2];
   }
 
   quotientDivisionPar5(minute) {
-      return Math.floor(minute / 5);
-    }
+    return Math.floor(minute / 5);
+  }
 
   resteDivisionPar5(minute) {
-      return minute % 5;
-    }
+    return minute % 5;
+  }
 }
+
+function main() {
+  const mainInstance = new Main();
+  mainInstance.startClock();
+}
+
+main();
